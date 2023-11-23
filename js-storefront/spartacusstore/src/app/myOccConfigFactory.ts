6660c90702353227b32a03cd87d9131f1939b133
Mohamed Anis Dkhil <mohamed.anis.dkhil@sap.com>
2023-11-23 00:20:39 -0500

    nat

--- js-storefront/spartacusstore/src/app/myOccConfigFactory.ts
+++ js-storefront/spartacusstore/src/app/myOccConfigFactory.ts
@@ -6,8 +6,8 @@ export function myOccConfigFactory(): OccConfig {
   const platformId = inject(PLATFORM_ID);
   const isBrowser = isPlatformBrowser(platformId);
   const baseUrl = isBrowser
-    ? 'https://api.clwfyft1x1-csamer2021-d29-public.model-t.cc.commerce.ondemand.com'
-    : 'https://api.clwfyft1x1-csamer2021-d29-public.model-t.cc.commerce.ondemand.com';
+    ? 'https://api.clwfyft1x1-csamer2021-d37-public.model-t.cc.commerce.ondemand.com'
+    : 'https://api.clwfyft1x1-csamer2021-d37-nat.model-t.cc.commerce.ondemand.com';
 
-    return { backend: { occ: { baseUrl: baseUrl }, media: { baseUrl: "https://api.clwfyft1x1-csamer2021-d29-public.model-t.cc.commerce.ondemand.com" } } };
+    return { backend: { occ: { baseUrl: baseUrl }, media: { baseUrl: "https://api.clwfyft1x1-csamer2021-d37-public.model-t.cc.commerce.ondemand.com" } } };
 }
